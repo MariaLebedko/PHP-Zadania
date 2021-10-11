0b6b99b442ffe3324aa5d1e7a948a259eb7b6e50
MariaLebedko <90271204+MariaLebedko@users.noreply.github.com>
2021-10-11 11:53:29 +0300

    Add files via upload

---
+++ P1.php
@@ -0,0 +1,16 @@
+<?php
+  $a = "blue";
+
+  if ($a == "purple"){
+    echo "фиолетовый";
+  }
+  elseif ($a == "yellow"){
+    echo "желтый";
+  }
+  elseif ($a == "blue"){
+    echo "голубой";
+    }
+  else{
+    echo ('Цвет не определен');
+  }
+ ?>
